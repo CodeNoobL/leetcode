4556c0ef225fc97953ca442df27b5d8897701eeb
CodeNoobL <1181671524@qq.com>
2018-05-26 15:06:17 +0800

    两数之和_已通过

--- src/leetcode/t1.java
+++ src/leetcode/t1.java
@@ -1,4 +1,5 @@
 package leetcode;
+
 /*
   给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。  你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
   示例:
@@ -8,19 +9,18 @@ package leetcode;
 所以返回 [0, 1]
  */
 public class t1 {
-	 public int[] twoSum(int[] nums, int target) {
-	        int[] result = new int[2];
-	        for(int i = 0;i<nums.length;i++)
-	        {
-	            for(int j =i+1;j<nums.length;j++){
-	                if(nums[i]+nums[j] == target){
-	                    result[0] = i;
-	                    result[1] = j;
-	                    return result;
-	                }
-	                }
-	                
-	        }
-	        return result;
-	    }
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+
+		}
+		return result;
+	}
 }
